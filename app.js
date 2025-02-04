c481efa4ba35f40c2fa84ac0925114e3cba464df
Shrihari25-hub <shriharibhil@gmail.com>
2025-02-05 02:39:36 +0530

    updates

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const cors = require('cors'); 
+const cors = require('cors');
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
@@ -12,12 +12,16 @@ const HttpError = require('./models/http-error');
 
 const app = express();
 
+// Enable CORS
 app.use(cors());
 
-app.use(bodyParser.json());
+// Parse JSON bodies (built-in with Express 4.16+)
+app.use(express.json());
 
+// Serve static files (images)
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
+// CORS headers (optional since you already use 'cors' middleware)
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
@@ -25,22 +29,24 @@ app.use((req, res, next) => {
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-
   next();
 });
 
+// Routes
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
+// 404 handler
 app.use((req, res, next) => {
   const error = new HttpError('Could not find this route.', 404);
   throw error;
 });
 
+// General error handler
 app.use((error, req, res, next) => {
   if (req.file) {
     fs.unlink(req.file.path, err => {
-      console.log(err);
+      if (err) console.log('Error removing file:', err);
     });
   }
   if (res.headerSent) {
@@ -50,14 +56,20 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
+// Fix the Mongoose deprecation warning
+mongoose.set('strictQuery', false);
 
+// Connect to MongoDB
 mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bdwqc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
   )
   .then(() => {
-    app.listen(5000);
+    console.log('Connected to MongoDB');
+    app.listen(5000, () => {
+      console.log('Server is running on port 5000');
+    });
   })
   .catch((err) => {
-    console.log(err);
+    console.log('MongoDB connection error:', err);
   });
